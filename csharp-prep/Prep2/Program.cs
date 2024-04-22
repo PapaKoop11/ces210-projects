6c35dd092049637c3071e998193347b483371e76
PapaKoop11 <papakoop@gmail.com>
2024-04-22 12:41:09 -0500

    Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string getVariable = Console.ReadLine();
+        int grade = int.Parse(getVariable);
+
+        string letterGrade = "";
+        string addOn = "";
+
+        if (grade >= 90) 
+        {            
+            letterGrade = "A";
+        }
+        else if (grade >= 80) 
+        {            
+            letterGrade = "B";
+        }
+        else if (grade >= 70) 
+        {            
+            letterGrade = "C";
+        }
+        else if (grade >= 60) 
+        {            
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (grade < 93 && grade >= 60)
+        {
+            if (grade%10 >= 7)
+            {
+                addOn = "+";
+            }
+            else if (grade%10 < 3)
+            {
+                addOn = "-";
+            }            
+        }
+
+        Console.WriteLine($"Your grade is {letterGrade + addOn}.");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrtulations! You passed this course.");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
+        
     }
 }
