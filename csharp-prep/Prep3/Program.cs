3041b43eaca6d36e313af5cc0d0af3daafc4dc84
PapaKoop11 <papakoop@gmail.com>
2024-04-26 07:57:52 -0500

    Finish prog 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0;
+        int i = 0;
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1, 99);
+
+    //    Console.WriteLine(magicNum);
+
+            while (magicNum != guess)
+            {
+                Console.Write("Try to guess the Magic Number. ");
+                string getReGuess = Console.ReadLine();
+                guess = int.Parse(getReGuess);
+
+                i++;
+
+                if (guess < magicNum)
+                {
+                    Console.WriteLine("Guess Higher");
+                }
+                else if (guess > magicNum)
+                {
+                    Console.WriteLine("Guess Lower");
+                }
+                else if (guess == magicNum)
+                {
+                    if (i == 1)
+                    {
+                        Console.WriteLine($"You guessed it in 1 try! Did you cheat?");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You guessed it in {i} tries!");
+                    }
+                }
+            }  
+        Console.Write("Would you like to play again? If so, type yes ");
+        playAgain = Console.ReadLine();       
+        }       
     }
 }
